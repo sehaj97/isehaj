9c2f20c19bd9e2b8281c7496f2addca8086eea8d
Sehajpreet Singh <sehajmagan@gmail.com>
2023-09-25 19:46:51 -0400

    updated isehaj react mvp

---
+++ src/components/Hero/index.js
@@ -0,0 +1,28 @@
+"use client";
+
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  width: 100vw;
+  height: 100vh;
+  background: papayawhip;
+`;
+
+function Hero() {
+  return (
+    <Wrapper>
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+        <p className="col-md-8 fs-4">
+          Using a series of utilities, you can create this jumbotron, just like
+          the one in previous versions of Bootstrap. Check out the examples
+          below for how you can remix and restyle it to your liking.
+        </p>
+        <button className="btn btn-primary btn-lg" type="button">
+          Example button
+        </button>
+      </div>
+    </Wrapper>
+  );
+}
+export default Hero;
