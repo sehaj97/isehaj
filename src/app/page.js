f2baccbecf55ac9f456ffd104c8c45c31b640fb8
Sehajpreet Singh <sehajmagan@gmail.com>
2023-09-25 21:13:28 -0400

    fixed content and style

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
+import CardListing from "@/components/CardListing";
 import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <CardListing />
+    </>
+  );
 }
---
+++ src/components/Card/index.js
@@ -0,0 +1,18 @@
+function Card() {
+  return (
+    <div className="card">
+      <img
+        src="https://cdn2.thecatapi.com/images/5cq.jpg"
+        class="card-img-top"
+        alt="..."
+      />
+      <div className="card-body">
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Card;
